1744cb588b25a19282cfcaedcd38061bcfcee3a8
Bala <22bgodavarthi@student.dasd.org>
2022-10-30 23:24:03 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>Baladev making monkey site</div>
+  )
+}
+
+export default App
