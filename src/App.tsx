c8ab27c8c494571ef49427d8c4d33082efd26647
Bala <22bgodavarthi@student.dasd.org>
2022-10-30 23:24:48 -0400

    monkey coma

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>Baladev making monkey site</div>
+    <div className='App'>Baladev making monkey site version 2.0</div>
   )
 }
 
