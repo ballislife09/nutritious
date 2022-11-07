6ff194ed4101589ecaaca6d6b1b462fad09d4574
nikita <nikita@vamrad.com>
2022-11-06 23:31:43 -0500

    cancer

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='background'>
+    <div className='mainpage'>
       <MenuBar/>
-      <div className='App'>Baladev making monkey site version 2.0</div>
+      <p style={{height:'3em'}}></p>
+      <button style={{margin:'2em', color:'#0bb2fa'}}>Find Our Monkey Cancer!</button>
     </div>
   )
 }
 
+{
+/* <p className='text large' style={{color:'#0bb2fa'}}>Eat Better Eat Monkey</p>
+<p className='text large' style={{color:'#05f941'}}>Find Your Meal Plan Today!</p>
+<p className='text medium otherfont' style={{color:'#32c675'}}>Specfic Meal Plans Created To Your Personal Needs</p> */}
 export default App
